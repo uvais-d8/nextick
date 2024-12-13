fa2a0472bfe1f237ded540528a0e1bd29f2923dd
Muhammed Uvais <muhammeduvais6060@gmail.com>
2024-12-13 16:28:11 +0530

    last

--- config/passport.js
+++ config/passport.js
@@ -1,47 +1,60 @@
-
 const passport = require("passport");
 const User = require("../model/usermodal");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const env = require("dotenv").config();
- 
+require("dotenv").config();
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      passReqToCallback: true, // Include req in the callback
     },
-    async (accessToken, refreshToken, profile, done) => {
+    async (req, accessToken, refreshToken, profile, done) => {
       try {
-        const existUser = await User.findOne({ googleId: profile.id });
-        console.log(existUser)
-        if (existUser) {
-          return done(null, existUser);
+        // Check if the user already exists in the database
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (user) {
+          // Existing user
+          console.log("Existing user found:", user);
+          return done(null, user);
         } else {
-          const newUser = await new User({
+          // Create a new user
+          const newUser = new User({
             name: profile.displayName,
             email: profile.emails[0].value,
-            googleId: profile.id
+            googleId: profile.id,
           });
+
           const savedUser = await newUser.save();
-          console.log(savedUser)
+          console.log("New user created:", savedUser);
+
+          // Store user ID in the session
           req.session.userId = savedUser._id;
           return done(null, savedUser);
         }
       } catch (error) {
+        console.error("Error in Google Strategy:", error);
         return done(error, null);
       }
     }
   )
 );
 
-passport.serializeUser((user, done) => [done(null, user.id)]);
+// Serialize user ID to save in session
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Deserialize user from session
 passport.deserializeUser((id, done) => {
   User.findById(id)
-    .then(user => {
+    .then((user) => {
       done(null, user);
     })
-    .catch(err => {
+    .catch((err) => {
       done(err, null);
     });
 });
