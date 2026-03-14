af7a6541e60bd41f776b002eee3093aeadfc5a39
Muhammed Uvais <muhammeduvais6060@gmail.com>
2026-03-14 16:30:10 +0530

    update

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,15 +1,15 @@
-const mongoose=require("mongoose")
-require("dotenv").config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectdb = async () => {
-    try {
-        await mongoose.connect("mongodb+srv://muhammeduvais:Hello786%40gmail.com@cluster0.q6tgs.mongodb.net/watchpremium?retryWrites=true&w=majority&appName=Cluster0", {
-        });
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.log('MongoDB connection error:', err);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log("MongoDB connection error:", err);
+  }
 };
-connectdb()
 
-module.exports =connectdb
+connectdb();
+
+module.exports = connectdb;
